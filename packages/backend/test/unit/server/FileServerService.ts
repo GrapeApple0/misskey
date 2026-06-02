549af3e2d4d35a12302f4c63d36e7aeaee588b51
GrapeApple0 <84321396+GrapeApple0@users.noreply.github.com>
2026-06-03 02:21:31 +0900

    remove AiService from test

--- packages/backend/test/unit/server/FileServerService.ts
+++ packages/backend/test/unit/server/FileServerService.ts
@@ -11,7 +11,6 @@ import { describe, expect, test, beforeAll, afterAll, afterEach } from 'vitest';
 import sharp from 'sharp';
 import { DataSource, type Repository } from 'typeorm';
 import { initTestDb, randomString } from '../../utils.js';
-import type { AiService } from '@/core/AiService.js';
 import { DownloadService } from '@/core/DownloadService.js';
 import { FileInfoService } from '@/core/FileInfoService.js';
 import { HttpRequestService } from '@/core/HttpRequestService.js';
@@ -147,10 +146,7 @@ describe('FileServerService', () => {
 		driveFilesRepository = db.getRepository(MiDriveFile);
 
 		const loggerService = new LoggerService();
-		const aiService = {
-			detectSensitive: async () => null,
-		} as unknown as AiService;
-		const fileInfoService = new FileInfoService(aiService, loggerService);
+		const fileInfoService = new FileInfoService(loggerService);
 		const httpRequestService = new HttpRequestService(config);
 		const downloadService = new DownloadService(config, httpRequestService, loggerService);
 		const imageProcessingService = new ImageProcessingService();
